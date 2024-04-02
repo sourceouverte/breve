06bf67bc70afd309bcda37bcef459b79b681742e
Maddy <nicemadhur@gmail.com>
2024-04-02 16:07:02 -0400

    Draft

---
+++ breve-ms/breve-url-ingester-service/src/main/java/org/breve/breveurlingesterservice/DemoApplication.java
@@ -0,0 +1,13 @@
+package org.breve.breveurlingesterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
