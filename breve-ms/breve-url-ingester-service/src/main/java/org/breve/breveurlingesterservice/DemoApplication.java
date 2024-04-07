35b6552925d04565fd0c5b94d81a84ae2ee1f0fd
Maddy <nicemadhur@gmail.com>
2024-04-06 23:36:53 -0400

    add greeting service to test functioning of a service

--- breve-ms/breve-url-ingester-service/src/main/java/org/breve/breveurlingesterservice/DemoApplication.java
+++ breve-ms/breve-url-ingester-service/src/main/java/org/breve/breveurlingesterservice/DemoApplication.java
@@ -2,8 +2,11 @@ package org.breve.breveurlingesterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.breve")
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
